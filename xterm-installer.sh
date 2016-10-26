6153d45ac1b31c9ef66532a0b5055f86417b181b
BlackCat <myugan59@gmail.com>
2016-10-26 21:37:05 +0700

    Add files via upload

---
+++ xterm-installer.sh
@@ -0,0 +1,12 @@
+wget ftp://invisible-island.net/xterm/xterm-327.tgz
+sed -i '/v0/{n;s/new:/new:kb=^?:/}' termcap &&
+printf '\tkbs=\\177,\n' >> terminfo &&
+
+TERMINFO=/usr/share/terminfo \
+./configure $XORG_CONFIG     \
+    --with-app-defaults=/etc/X11/app-defaults &&
+
+make
+make install &&
+make install-ti
+echo "Success install xterm"
